3b9a56f17504e5da39c17526041ca0154d739487
Håkan Eriksson <haken.eriksson@gmail.com>
2013-09-17 14:49:52 +0200

    Kind of working, with sound notification.

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 mcs-login-alert
 ===============
 
-A Bukkit plugin for Minecraft servers (on Raspberry Pi, primarily) that notifies the host when a user logs in (or out). 
+A Bukkit plugin for Minecraft servers (on Raspberry Pi, primarily) that 
+notifies the host when a user logs in (or out). 
+
+The doorbell sound is a free sound effect taken from the site
+http://www.freespecialeffects.co.uk
+
--- src/io/github/barandc/loginalert/LoginAlert.java
+++ src/io/github/barandc/loginalert/LoginAlert.java
@@ -1,7 +1,15 @@
 package io.github.barandc.loginalert;
 
+import java.net.URL;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
+import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -19,6 +27,9 @@ public final class LoginAlert extends JavaPlugin implements ILoginHandler, Liste
 	public void onEnable()
 	{
 		getLogger().info("LoginAlert is enabled.");
+
+		getServer().getPluginManager().registerEvents(this, this);
+
 		handlePlayersOnServer(getServer().getOnlinePlayers().length);
 	}
 	
@@ -26,30 +37,73 @@ public final class LoginAlert extends JavaPlugin implements ILoginHandler, Liste
 	public void onDisable()
 	{
 		getLogger().info("LoginAlert is disabled.");
+
+		HandlerList.unregisterAll((Listener)this);
+
+		handlePlayersOnServer(0);
 	}
 
 	@EventHandler(priority = EventPriority.LOW)
-	public void onPlayerJoin(PlayerJoinEvent event) {
+	public void onPlayerJoin(PlayerJoinEvent event) 
+	{
 		//Player player = event.getPlayer();
 		//player.sendMessage("Hello Sir!");
 		handlePlayersOnServer(getServer().getOnlinePlayers().length);
 	}
 	
 	@EventHandler(priority = EventPriority.LOW)
-	public void onPlayerQuit(PlayerQuitEvent event) {
-		handlePlayersOnServer(getServer().getOnlinePlayers().length);
+	public void onPlayerQuit(PlayerQuitEvent event) 
+	{
+		/* For some odd reason, the PlayerQuitEvent seems to be thrown while the player 
+		 * still is in the list of online players. I found no event thrown after a player 
+		 * logged out. I might tackle it otherwise in a future implementation, but for now 
+		 * I simply pass "the LoginHandler" the number of players excluding the one logging
+		 * out. */
+		Player[] players = getServer().getOnlinePlayers();
+		int numberOfPlayersInList = players.length;
+		int numberOfPlayersLeft = 0; 
+		for (int i = 0; i < numberOfPlayersInList; i++)
+		{
+			// Count all those players whose name differs from the quitter's.
+			if (players[i].getName() != event.getPlayer().getName())
+			{
+				numberOfPlayersLeft++;
+			}
+		}
+		handlePlayersOnServer(numberOfPlayersLeft);
 	}
 
 	
 	@Override
-	public void handlePlayersOnServer(int numberOfPlayers) {
+	public void handlePlayersOnServer(int numberOfPlayers) 
+	{
 		if (numberOfPlayers > 0)
 		{
 			getLogger().info("There are players online!");
+			playPlayerOnlineSound();
 		}
 		else
 		{
 			getLogger().info("There are NO players online!");
+			// TODO: Play "crickets" sound? 
+		}
+	}
+
+	private void playPlayerOnlineSound() 
+	{
+		try
+		{
+			URL soundURL = getClass().getClassLoader().getResource("doorbell2.wav");
+
+			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(soundURL);
+			Clip clip = AudioSystem.getClip();
+			clip.setFramePosition(0);
+			clip.open(audioInputStream);
+			clip.start();
+		}
+		catch (Exception ex)
+		{
+			getLogger().info("ERROR: failed to play login sound 2: " + ex.getMessage());
 		}
 	}
 }
